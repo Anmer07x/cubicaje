b14e500715e48a38a09385d332636813f16a505b
Anmer07x <115746770+Anmer07x@users.noreply.github.com>
2024-04-11 17:26:55 -0500

    Cambios

--- login.php
+++ login.php
@@ -11,25 +11,29 @@
 <body>
     <div class="header">
         <div class="logo-container">
-            <div class="initials">SCUa</div>
+            <div class="initials">SCU</div>
             <div class="bar"></div>
             <img src="img/logoU.png" alt="Logo">
         </div>
     </div>
     <div class="body-background"></div>
-        <form class="containerLogin" action="">
-            <img class="logoLogin" src="img/logoU.png" alt=""> 
-            <input class="input" type="email" placeholder="Correo electrónico:">
-            <input class="input" type="password" placeholder="Contraseña:">
-            <div class="contenedor">
-                <div class="izquierda">
-                    <input class="check" type="checkbox">Mantener sesión activa
-                </div>
-                <div class="derecha"><link rel="stylesheet" href=""> ¿Olvidaste tu contraseña?</div>   
-            </div>
-            <button class="buttonSubmit" type="submit">Iniciar sesión</button>
+    <form class="containerLogin" action="">
+    <img class="logoLogin" src="img/logoU.png" alt=""> 
+    <input class="input" type="email" placeholder="Correo electrónico:">
+    <input class="input" type="password" placeholder="Contraseña:">
+    <div class="contenedor">
+        <div class="izquierda">
+            <input class="check" type="checkbox">Mantener sesión activa
+        </div>
+        <div class="derecha">
+            <a href="reestablecer.php">¿Olvidaste tu contraseña?</a> <!-- Corregido -->
+        </div>   
+    </div>
+    <a href="index.php"> <!-- Corregido -->
+        <button class="buttonSubmit" type="submit">Iniciar sesión</button>
+    </a>
+</form>
 
-        </form>
     <div class="help-box">
         <a href="#">
             <i class="fas fa-question-circle"></i> Ayuda
