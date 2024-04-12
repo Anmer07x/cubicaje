92fe8ec775c83b8c1042d1e0d5cead252a79f0b7
Anmer07x <115746770+Anmer07x@users.noreply.github.com>
2024-04-12 12:26:43 -0500

    Correciones al CSS

--- pages/login.php
+++ pages/login.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="../css/style.css">
-    <link rel="icon" type="image/x-icon" href="img/pila-de-cubos.png">
+    <link rel="icon" type="image/x-icon" href="../img/pila-de-cubos.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
@@ -17,19 +17,21 @@
         </div>
     </div>
     <div class="body-background"></div>
-        <form class="containerLogin" action="">
-            <img class="logoLogin" src="../img/logoU.png" alt=""> 
-            <input class="input" type="email" placeholder="Correo electrónico:">
-            <input class="input" type="password" placeholder="Contraseña:">
-            <div class="contenedor">
-                <div class="izquierda">
-                    <input class="check" type="checkbox">Mantener sesión activa
-                </div>
-                <div class="derecha"><link rel="stylesheet" href=""> ¿Olvidaste tu contraseña?</div>   
-            </div>
-            <button class="buttonSubmit" type="submit">Iniciar sesión</button>
+    <form class="containerLogin" action="">
+    <img class="logoLogin" src="../img/logoU.png" alt=""> 
+    <input class="input" type="email" placeholder="Correo electrónico:">
+    <input class="input" type="password" placeholder="Contraseña:">
+    <div class="contenedor">
+        <div class="izquierda">
+            <input class="check" type="checkbox">Mantener sesión activa
+        </div>
+        <div class="derecha">
+            <a href="reestablecer.php">¿Olvidaste tu contraseña?</a> <!-- Corregido -->
+        </div>   
+    </div>
+    <button class="buttonSubmit" type="submit">Iniciar sesión</button>
+</form>
 
-        </form>
     <div class="help-box">
         <a href="#">
             <i class="fas fa-question-circle"></i> Ayuda
